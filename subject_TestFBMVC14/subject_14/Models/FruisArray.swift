94316c187448abdb0bf66e02d90a709fd3739347
Hase401 <ponta410422@icloud.com>
2021-06-25 22:29:22 +0900

    後からテストコード用ファイルを追加

---
+++ subject_TestFBMVC14/subject_14/Models/FruisArray.swift
@@ -0,0 +1,19 @@
+//
+//  FruisArray.swift
+//  subject_14
+//
+//  Created by 長谷川孝太 on 2021/06/18.
+//
+
+// 【削除】初期化としてViewControllerに設定した
+
+// import Foundation
+
+// final class FruitsArray {
+//    var fruits = [
+//        Fruit(isChecked: false, name: "りんご"),
+//        Fruit(isChecked: true, name: "みかん"),
+//        Fruit(isChecked: false, name: "バナナ"),
+//        Fruit(isChecked: true, name: "パイナップル")
+//    ]
+// }
